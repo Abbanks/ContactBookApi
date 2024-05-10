72df6a92b4781f6c418816094be71c7a21828009
Oluwabanke Eboda <abeegarl@gmail.com>
2024-05-11 00:24:45 +0100

    Update

--- obj/Debug/net8.0/ContactPageApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/ContactPageApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContactPageApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19ae5ad05a2f21329d8adb8e9a9e21c4d9b2b6ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+731da244e8e3a994bda8e679cb8d18ffe9fead2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContactPageApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContactPageApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
